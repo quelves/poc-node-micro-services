a07a4d1584339f5c81ac1c469b0a9cc6b8edb0f3
quelves <luiz.quelves@gmail.com>
2017-12-19 18:53:20 -0300

    add express template

--- etc/docker/run_node_dev.sh
+++ etc/docker/run_node_dev.sh
@@ -5,8 +5,8 @@ docker stop dc-node-1
 docker rm dc-node-1
 
 docker run -d -i -t --name dc-node-1 -h dc-node-1 \
-  -p 4200:4200 \
-  -p 3000:3000 \
+  -p 14200:4200 \
+  -p 13000:3000 \
   -v $(pwd)/../../:/app \
   quelves/dev:node
 
